635b7eb23696dab7cdab92ce80d236efb305e1f8
Theo Sun <theo.sun@outlook.com>
2020-07-31 19:48:15 +0800

    chore(unit): test update

--- test/odata-uri.spec.js
+++ test/odata-uri.spec.js
@@ -1,14 +1,23 @@
 import { Parser } from "../src/parser";
+import { ServiceMetadata } from "@odata/metadata";
 
 
 describe('OData URI Test Suite', () => {
+
+    const schoolMeta = ServiceMetadata.processMetadataJson(require("./resources/school.edmx.json"))
+
     const parser = new Parser()
 
     it('should parser uri', () => {
-        
+
+
+
         parser.odataUri("/Categories?$skip=30")
         parser.odataUri("/Categories(10)?$expand=A,C&$select=D,E")
+        const ast = parser.odataUri("/Classes?$expand=students/student", { metadata: schoolMeta.edmx })
 
+        expect(ast).not.toBeUndefined()
+    
     });
 
 });
