88e5bf25a084fb389879088f468727befe6c8e70
Theo Sun <theo.sun@outlook.com>
2020-08-09 08:48:02 +0800

    feat: type for batch request for odata 4.01

---
+++ src/builder/batch.ts
@@ -0,0 +1,21 @@
+// https://docs.oasis-open.org/odata/odata-json-format/v4.01/odata-json-format-v4.01.html#_Toc38457781
+// support json format for batch request
+
+export interface JsonBatchBundle {
+    requests: JsonBatchRequest[];
+}
+
+export type JsonBatchMethod = 'get' | 'post' | 'put' | 'patch' | 'delete'
+
+export type JsonBatchHeaders = Record<string, string>
+
+export interface JsonBatchRequest<T = any> {
+    id: string;
+    method: JsonBatchMethod;
+    url: string;
+    atomicityGroup?: string;
+    dependsOn?: string[];
+    headers?: JsonBatchHeaders;
+    body?: T;
+}
+
--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,3 +1,6 @@
+import { createRequireFromPath } from 'module';
+
 export * from './filter';
 export * from './param';
 export * from './types';
+export * from './batch';
