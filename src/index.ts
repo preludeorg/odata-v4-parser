f3c1896808a360a0caee75150dd9bdd2e22597b2
Theo Sun <theo.sun@sap.com>
2020-08-26 16:08:08 +0800

    chore: add type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Parser } from './parser';
 
 export * from './builder';
 export * from './lexer';
+export * from './types';
 export * from './visitor';
 
 export const defaultParser = new Parser();
